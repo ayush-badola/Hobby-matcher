824ce7c0a18d0bcf4824a1d310a9678c634a5c6c
Charu-saraswat <charusaraswat639649@gmail.com>
2025-02-17 16:21:59 +0530

    Added new feature or code

--- hobby-matcher/frontend/vite.config.js
+++ hobby-matcher/frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+ 
+    server: {
+      host: true, // or specify the exact host
+      preview: {
+        allowedHosts: ['hobby-matcher-9-a00h.onrender.com'],
+      }
+    }
+
 })
+
