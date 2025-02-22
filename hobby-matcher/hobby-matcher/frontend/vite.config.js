1afa5d46ae518a56530ffa213da69d77cfc7e1a5
Charu-saraswat <charusaraswat639649@gmail.com>
2025-02-22 08:48:11 +0530

    Minor changes

--- hobby-matcher/hobby-matcher/frontend/vite.config.js
+++ hobby-matcher/hobby-matcher/frontend/vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    host: true,
+    host: true, // Allow external access
   },
+
   preview: {
     allowedHosts: ['hobby-matcher-9-a0oh.onrender.com'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  }
+
 })
